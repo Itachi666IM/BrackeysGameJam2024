daf4958614640aa3aeac0e8a4907e38f8a975553
Sanj1t <79507387+Itachi666IM@users.noreply.github.com>
2024-09-09 13:57:31 +0530

    Added Assets, BGM and Button SFX

---
+++ Calm Before The Storm/Assets/Scripts/ClickSound.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickSound : MonoBehaviour
+{
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip buttonClick;
+    public void PlayButtonClickSound()
+    {
+        audioSource.PlayOneShot(buttonClick);
+    }
+}
